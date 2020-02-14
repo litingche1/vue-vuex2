4ecf4f1760d04392507f01a064af29e1271f7f82
李廷琛 <2496272528@qq.com>
2020-02-14 15:23:09 +0800

    添加注释

--- src/views/About.vue
+++ src/views/About.vue
@@ -21,6 +21,7 @@ export default {
     
   },
   computed: {
+    //vuex中的store分模块管理，需要在store的index.js中引入各个模块，为了解决不同模块命名冲突的问题，将不同模块的namespaced:true，之后在不同页面中引入getter、actions、mutations时，需要加上所属的模块
     // ...mapGetters(['getMMNAME','getMPNAMES','getMSNAMES'])
     ...mapGetters({getMSNAMES: 'ms/getMSNAMES',getMPNAMES: 'mp/getMPNAMES',getMPNAMES2: 'MP2/getMPNAMES'})
     
